741f78c1b25d8a13d309490dee91f8c28c9f4335
NiteshSSi <nitesh.ssiedu@gmail.com>
2022-05-16 19:07:44 +0530

    Create example.py

---
+++ example.py
@@ -0,0 +1,20 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Welcome to Ssi Digital Academy ,Indore")
+Welcome to Ssi Digital Academy ,Indore
+>>> print("Python Programming Language")
+Python Programming Language
+>>> print("Batch Time 6 PM to 7 PM")
+Batch Time 6 PM to 7 PM
+>>> 5+6
+11
+>>> 5*9
+45
+>>> 5+9*9+7-5
+88
+>>> 
+==== RESTART: E:/Nitesh Jain/Python/PY120522-6PM/PY120522-6PM/Welcome.py ====
+Welcome to Ssi Digital Academy , Indore
+Python Programming Language
+Batch Time 6 PM to 7 PM
+>>> 
