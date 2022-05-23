c8c6eeccf255e110506f481cf7ff335333b0db9d
NiteshSSi <nitesh.ssiedu@gmail.com>
2022-05-23 16:13:44 +0530

    Create mul.py
     Multiplication of Two Number

---
+++ mul.py
@@ -0,0 +1,4 @@
+#a=eval(input("Enter the value of  a :- "))
+#b=eval(input("Enter the value of b :- "))
+#mul=a*b    #Multiplication of Two number
+#print("Multiplication of Two Number  :- ",mul)  # Print Result
